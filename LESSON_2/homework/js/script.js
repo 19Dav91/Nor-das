e4ff0a9a571de41489430bf83394f77a51035cd1
19Dav91 <johndoe@example.comdmatevosyan91@mail.ru>
2023-04-02 19:15:58 +0400

    Das

--- LESSON_2/homework/js/script.js
+++ LESSON_2/homework/js/script.js
@@ -392,18 +392,18 @@
 // } else {
 // 	console.log('язык не поддерживается');
 // }
-let lang = 'ru';
-switch (lang) {
-    case 'ru':
-        console.log('рус');
-        break;
-    case 'en':
-        console.log('анг');
-        break;
-    case 'de':
-        console.log('нем');
-        break;
-    default:
-        console.log('язык не поддерживается');
-        break;
-}
+// let lang = 'ru';
+// switch (lang) {
+//     case 'ru':
+//         console.log('рус');
+//         break;
+//     case 'en':
+//         console.log('анг');
+//         break;
+//     case 'de':
+//         console.log('нем');
+//         break;
+//     default:
+//         console.log('язык не поддерживается');
+//         break;
+// }
