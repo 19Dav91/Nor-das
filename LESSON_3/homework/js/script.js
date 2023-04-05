3ed4f0220188401b7e78e469f6ce2e3fac99d175
19Dav91 <johndoe@example.comdmatevosyan91@mail.ru>
2023-04-05 22:40:38 +0400

    Das

--- LESSON_3/homework/js/script.js
+++ LESSON_3/homework/js/script.js
@@ -184,3 +184,11 @@
 //         console.log(str);
 //     }
 // }
+
+let str = '';
+for (let i = 0; i <= 3; i++) {
+    for (let j = 0; j <= 3; j++) {
+        let result = String(i) + String(i) + ''        
+    }
+    console.log(result);
+}
