0f9efa2bff3fc86d5da65edc15dcacfb358ea0d9
19Dav91 <johndoe@example.comdmatevosyan91@mail.ru>
2023-04-07 10:36:53 +0400

    ddd

---
+++ LESSON_4/das/js/script.js
@@ -0,0 +1,157 @@
+
+// ?????????????????????/
+// let num = 33; 
+// let sum = 0; 
+// let count = 0; 
+// while (sum < 1000) { 
+//     sum = (num * 3); 
+//     num++; 
+//     count++ 
+// } 
+// console.log(sum-3); 
+//console.log(count);
+
+//????????????????????????
+// let num = 2;
+// let count = 0;
+// let sum=0;
+// while (num < 1000) {
+    
+//     console.log(sum);
+//     sum = num *= 3;
+//     count++
+// }
+// console.log(count);
+
+
+
+
+
+// let arr = ['a', 'b', 'c', 'd'];
+// let result = '';
+// for (let i = 0; i < arr.length; i++) {
+//   result += arr[i];
+//   if (i < arr.length - 1) {
+//     result += '+';
+//   }
+// }
+// console.log(result); 
+
+// ///////
+// let arr = ['a', 'b', 'c', 'd'];
+// let str1 = ''
+// for (let elem of str) {
+//     if (elem !== str[str.length-1]) {
+//         str1 += elem + '+';
+//     } else{
+//         str1 += str[str.length-1]
+//     }
+// }
+// console.log(str1);
+
+
+// выведет 'a+b+c+d'
+
+//////////////
+// delete arr;
+// console.log(arr);
+////////////////objects
+//  let obj = {'a': 'd', 2: 'b', 3: 'c'};
+//  let a=3;
+// alert(obj.a); //'d'
+// alert(obj[a]);//'c'
+////////////////////
+
+//length in obj is let keys = Object.keys(obj);
+
+//???????????????
+// let obj = {x: 1, y: 2, z: 3};
+// let keys = Object.keys(arr);
+// console.log(keys);
+//
+// let obj = {x: 1, y: 2, z: 3};
+// let key = 'x';
+// console.log(obj[key]);
+
+////////////////
+// let arr1 = [1, 2, 3];
+// let arr2 = arr1;
+
+// arr1[0] = 'a';
+// console.log(arr2);
+/////////////////
+
+
+
+
+
+
+//
+// for(let i = 1, j=2, k=5;i < 15;i++, j+=2,k+=5){
+      
+//     console.log(i,j,k);
+// }
+//
+// for(let i = 1, j=1,k=1;i < 20; i++, j++,k++){
+      
+//     console.log(i + j+k);
+// }
+
+
+
+
+
+
+
+////////////////////////
+// let arr = ['a', 'b', 'c', 'a', 'a', 'b']; 
+// let counter = 0;
+
+// for (let elem of arr) {
+// 	if (elem == 'a') {
+// 		counter++;
+// 	}
+// }
+// console.log(counter);
+//////////////////////
+// let arr = ['a', 'b', 'c', 'a', 'a', 'b']; 
+// let count = {a: 0, b: 0, c: 0};
+// for (let elem of arr) {
+// 	count[elem]++;
+// }
+// console.log(count); 
+// let arr = ['a', 'b', 'c', 'a', 'a', 'b']; 
+// let count = {'d':0,'a':6};
+
+// for (let elem of arr) {
+// 	if (count[elem] === undefined) {
+// 		count[elem] = 1;
+// 	} else {
+// 		count[elem]++;
+// 	}
+// }
+//console.log(count); 
+
+///////////////////////////////
+// let arr = [
+// 	[
+// 		['a', 'b'],
+// 		['c', 'd'],
+// 	],
+// 	[
+// 		['e', 'f'],
+// 		['g', 'h'],
+// 	],
+// 	[
+// 		['i', 'j'],
+// 		['k', 'l'],
+// 	],
+// ];
+
+
+//////////////////
+ let arr = [['a', 'b', [1, 2, 3], [4, 5]], ['d', ['e', 'f']]];
+
+
+
+
