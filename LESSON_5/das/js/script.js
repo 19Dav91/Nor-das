c1ac3ab0add9f93f04007d91ab46cf9bbd25925f
19Dav91 <johndoe@example.comdmatevosyan91@mail.ru>
2023-04-12 22:28:17 +0400

    das

--- LESSON_5/das/js/script.js
+++ LESSON_5/das/js/script.js
@@ -11,14 +11,14 @@
 // 	],
 // ];
 ///////////////////
-//let arr = [[1, 2, 3, 4, 5], [6, 7, 8], [9, 
-// 	10]]; 
+let arr = [[1, 2, 3, 4, 5], [6, 7, 8], [9, 
+	10]]; 
 
-// for (let subArr of arr) {
-// 	for (let elem of subArr) {
-// 		console.log(elem);
-// 	}
-// }
+for (let subArr of arr) {
+	for (let elem of subArr) {
+		console.log(elem);
+	}
+}
 //////////////////////////////
 // let arr = [];
 
