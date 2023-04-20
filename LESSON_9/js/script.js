c9e59a2d1aab095144b659c9968dcf371b2745de
19Dav91 <johndoe@example.comdmatevosyan91@mail.ru>
2023-04-20 21:48:47 +0400

    Das

--- LESSON_9/js/script.js
+++ LESSON_9/js/script.js
@@ -52,3 +52,16 @@
 // }
 // console.log(sum([1, 2, 3, 4, 5]));
 
+// return fibonacci(n - 1) + fibonacci(n - 2);
+// function fibonacci(n) {
+//     if (n == 0) {
+//         return [0];
+//     } else if (n == 1) {
+//         return [0, 1]
+//     } else {
+//         let arr = fibonacci(n - 1)
+//         arr.push(arr[arr.length - 1] + arr[arr.length - 2])
+//         return arr
+//     }
+// }
+// console.log(fibonacci(2));
