445eac7f94f4eee7ea836967f5ac03ad14b36745
19Dav91 <johndoe@example.comdmatevosyan91@mail.ru>
2023-04-20 21:48:07 +0400

    Das

---
+++ LESSON_9/js/script.js
@@ -0,0 +1,54 @@
+// function fibonacci(n) {
+//     if (n <= 1) return n;
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+//   }
+//     console.log(fibonacci(7));
+//     let arr = [];
+
+// function pow(x, y) {
+//     if (y == 1) {
+//         return x
+//     } else {
+//         x *= pow(x, y - 1)
+//         return x
+//     }
+// }
+// console.log(pow(2, 3));
+
+
+// function sum(a) {
+//     let res = 1;
+//     for (let i = 1; i <= a; i++) {
+//         res *= i
+//     }
+//     return res
+// }
+// console.log(sum(5));
+
+// function fakt(n) {
+//     if (n == 1 ||n == 0) {
+//         return n
+//     } else {
+//         n *= fakt(n - 1)
+//     }
+//     return n
+// }
+// console.log(fakt(0));
+
+
+
+
+
+
+
+
+
+// function sum(arr) {
+//     let res = 0;
+//     for (let elem of arr) {
+//         res += elem;
+//     }
+//     return res;
+// }
+// console.log(sum([1, 2, 3, 4, 5]));
+
