02dbc45f97e6caf3be4e5fd0dd5218b87e43b681
19Dav91 <johndoe@example.comdmatevosyan91@mail.ru>
2023-04-22 21:15:25 +0400

    Das

---
+++ LESSON_10/Das/js/script.js
@@ -0,0 +1,18 @@
+// let arr = [1, 2, 3, 4, 5];
+// function sqrtSum(arg) {
+//     let res = arg.shift();
+//     let sum = Math.pow(res,2);
+//     if (arg.length != 0) {
+//         sum += sqrtSum(arg)
+//     }
+//     return sum
+// }
+// console.log(sqrtSum(arr));
+
+// function sumTo(n) {
+//     if (n == 1) {
+//         return n
+//     }
+//     return n + sumTo(n - 1)
+// }
+// console.log(sumTo(100));
