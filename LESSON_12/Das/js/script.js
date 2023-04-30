aed30a88061e54914abab4b8caf46c9ed4ce3837
19Dav91 <dmatevosyan91@mail.ru>
2023-04-30 21:35:09 +0400

    Das

--- LESSON_12/Das/js/script.js
+++ LESSON_12/Das/js/script.js
@@ -53,20 +53,20 @@
 // str(['armen','hayk', 'david'],'/')
 // console.log(str(['armen','hayk', 'david'],'/'));
 
-function foo(x, y) {
-    let arr = [];
-//     let ind = x.indexOf(y);
-    // let res = x.slice(0, ind)
-    // let res1 = x.slice(ind + 1)
-    // arr.push(res + res1)
-    for (const elem of x) {
-        if (elem.indexOf(y) === '@') {
-            elem.spice(0,y)
-            console.log(elem);
-        }else {
-            arr.push(elem)
-        }
-    }
-    return arr
-}
-console.log(foo('gmail.@gmail.com', '@'))
+// function foo(x, y) {
+//     let arr = [];
+// //     let ind = x.indexOf(y);
+//     // let res = x.slice(0, ind)
+//     // let res1 = x.slice(ind + 1)
+//     // arr.push(res + res1)
+//     for (const elem of x) {
+//         if (elem.indexOf(y) === '@') {
+//             elem.spice(0,y)
+//             console.log(elem);
+//         }else {
+//             arr.push(elem)
+//         }
+//     }
+//     return arr
+// }
+// console.log(foo('gmail.@gmail.com', '@'))
