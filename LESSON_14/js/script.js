edbb0b28f770153adfac4d87924895c2241f4677
19Dav91 <dmatevosyan91@mail.ru>
2023-05-04 16:42:41 +0400

    DAs

---
+++ LESSON_14/js/script.js
@@ -0,0 +1,131 @@
+// Input-ում մուտքագրվում է րոպեների քանակը, button-ի click-ի ժամանակ
+// հաշվել թե քանի ժամ է կազմում մուտքագրված թվանշանը և h1-ի մեջ տպել այն
+// Առաջադրանք 3: Ունենք input դաշտ և button`
+
+
+///////////////
+// let button = document.querySelector('#btn');
+// let inp = document.querySelector('#name');
+// let text = document.querySelector('#txt');
+// button.addEventListener('click', function func() {
+// text.textContent = `Hello ${inp.value}`
+// });
+/////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let button = document.querySelector('#button');
+// button.addEventListener('click', func);
+
+// function func() {
+// 	console.log('!!!');
+// }
+// button.addEventListener('mouseover', function() {
+// 	console.log('1');
+// });
+// button.addEventListener('mouseout', function() {
+// 	console.log('2');
+// });
+////////////////TEXT
+// let elem = document.querySelector('#elem');
+// console.log(elem.textContent);
+// document.write(elem.textContent)
+// document.write(elem.innerText)
+// elem.innerHTML+= `<i>italic</i>`;
+// elem.innerHTML += '!!!';
+
+// // innerHTML
+// console.log(elem.innerHTML);
+// elem.innerHTML = '<i>!!!</i>';
+////////////ATTRIBUTE
+// let elem = document.querySelector('#elem');
+//  console.log(elem.id);  
+// console.log(elem.type); 
+// console.log(elem.className); 
+//elem.type = 'submit';
+// console.log(elem.value);
+//  elem.value = 'new text';
+// elem.addEventListener('focus', function() {
+// 	console.log(elem.value);
+// });
+// //////class
+// console.log(elem.className);
+//////////////
+// //document.querySelector('#elem').value = 'www';
+//////this
+
+// elem.addEventListener('blur', func);
+// function func() {
+// 	console.log(this); 
+//     // console.log(this.value);
+//     // this.value="!!!"
+// }
+// let button1 = document.querySelector('#button1');
+// let button2 = document.querySelector('#button2');
+// let button3 = document.querySelector('#button3');
+
+
+// button1.addEventListener('click', func);
+// button2.addEventListener('click', func);
+// button3.addEventListener('click', func);
+// function func() {
+// 	console.log(this.value);
+// }
+/////////////////////////querySelectorAll
+// let elems = document.querySelectorAll('.www');
+// console.log(elems);
+// for (let elem of elems) {
+// 	//console.log(elem.textContent);
+//     elem.textContent = elem.textContent + '!'; 
+// }
+// ////////////////////
+// for (let elem of elems) {
+// 	elem.addEventListener('click', func);
+// }
+// function func() {
+	
+//     console.log(this.textContent);
+// }
+//////////////
+// for (let elem of elems) {
+// 	elem.addEventListener('click', function() {
+// 		console.log(this.textContent);
+// 	});
+// }
+//////////////removeEventListener
+// let button = document.querySelector('#button');
+// button.addEventListener('click', func);
+// let count=0;
+// function func() {
+// 	console.log('!!!');
+//     count++;
+// 	this.removeEventListener('click', func);
+//  }//?????????
+////getAttribute
+/////setAttribute
+////removeAttribute
+////hasAttriubute
+//classList.contains
+//classList.add
+//classList.remove
+///classList.toggle
+
+
+
+
