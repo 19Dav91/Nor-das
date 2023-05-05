b9df280cd651858f196b80bb989f9e8045de5d04
19Dav91 <dmatevosyan91@mail.ru>
2023-05-05 17:02:44 +0400

    Das

--- LESSON_14/js/script.js
+++ LESSON_14/js/script.js
@@ -1,4 +1,4 @@
-// Input-ում մուտքագրվում է րոպեների քանակը, button-ի click-ի ժամանակ
+//1 Input-ում մուտքագրվում է րոպեների քանակը, button-ի click-ի ժամանակ
 // հաշվել թե քանի ժամ է կազմում մուտքագրված թվանշանը և h1-ի մեջ տպել այն
 // Առաջադրանք 3: Ունենք input դաշտ և button`
 
@@ -11,6 +11,124 @@
 // text.textContent = `Hello ${inp.value}`
 // });
 /////////////
+
+// 2 Input-ում մուտքագրվում է րոպեների քանակը, button-ի click-ի ժամանակ
+// հաշվել թե քանի ժամ է կազմում մուտքագրված թվանշանը և h1-ի մեջ տպել այն
+
+// let button = document.querySelector('#btn');
+// let inp = document.querySelector('#name');
+// let text = document.querySelector('#txt');
+// button.addEventListener('click', function func() {
+// text.textContent = inp.value /60
+// });
+
+
+// 3 Input-ում մուտքագրվում է թվանշան(դրամ), button-ի click-ի ժամանակ հաշվել
+// թե քանի դոլլար է կազմում մուտքագրված թվանշանը և h1-ի մեջ տպել այն
+
+// let button = document.querySelector('#btn');
+// let inp = document.querySelector('#name');
+// let text = document.querySelector('#txt');
+// button.addEventListener('click', function func() {
+// text.textContent = inp.value / 389
+// });
+
+// 4 Առաջին Input դաշտում մուտքագրվում թիվը, իսկ երկրորդում թվի աստիճանը,
+// button-ի click-ի ժամանակ հաշվել մուտքագրված թվի համապատասխան
+// աստիճանը
+
+// let button = document.querySelector('#btn');
+// let inp = document.querySelector('#name');
+// let inp1 = document.querySelector('#name1');
+// let text = document.querySelector('#txt');
+
+// button.addEventListener('click', function func() {
+// text.textContent = Number(inp.value) + Number(inp1.value)
+// });
+
+// 4 
+// ❖ Input դաշտերում մուտքագրվում են թվեր: Առաջին դաշտում մուտքագրվում է
+// թիվը, իսկ երկրորդում տոկոսը ։ Button -ի քլիկի ժամանակ ցուցադրել թվի
+// տոկոսը
+// ★ Օրինակ՝ 150 000 և 50 - ի դեպքում կցուցադրվի 150 000-ի 50 % - ը,
+// այսինքն` 150000*50/100 = 7500։
+
+
+// let button = document.querySelector('#btn');
+// let inp = document.querySelector('#name');
+// let inp1 = document.querySelector('#name1');
+// let text = document.querySelector('#txt');
+// button.addEventListener('click', function func() {
+// text.textContent = inp.value * (inp1.value/100)
+// });
+
+
+// 6.Input դաշտում մուտքագրվում է վայրկյանների քանակը, button-ի click-ի
+// ժամանակ մուտքագրված վայրկյանները ձևափոխել օր/ժամ/րոպե/վայրկյան
+// տեսքի և տպել h1-ի մեջ.
+// Օրինակ՝ 187801 վայրկյանի դեպքում արդյունքը կլինի՝ 2 օր, 4
+// ժամ, 10 րոպե, 1 վայրկյան
+// let seconds = inp.value;
+// let days = Math.floor(seconds / 86400);
+// seconds -= days * 86400;
+// let hours = Math.floor(seconds / 3600);
+// seconds -= hours * 3600;
+// let minutes = Math.floor(seconds / 60);
+// seconds -= minutes * 60;
+
+// text.textContent = `${days}or, ${hours}jam, ${minutes}rope, ${seconds}varkyan`;
+
+let button = document.querySelector('#btn');
+let inp = document.querySelector('#name');
+let inp1 = document.querySelector('#name1');
+let text = document.querySelector('#txt');
+     utton.addEventListener('click', function func() {
+    let seconds = inp.value;
+let days = Math.ceil(seconds / 86400);
+seconds -= days * 86400;
+let hours = Math.floor(seconds / 3600);
+                  seconds -= hours * 3600;
+let minutes = Math.floor(seconds / 60);
+seconds -= minutes * 60;
+
+text.textContent = `${days}or, ${hours}jam, ${minutes}rope, ${seconds}varkyan`;
+
+// let day = Math.floor(inp.value / 24 /60 /60);
+// let jam = Math.floor((+inp.value - (day*24*60*60))/60/60 );
+// let rope = ((+inp.value - (day*24*60*60))/60/60);
+// let sec = rope/60;
+//     text.textContent = `${day}or,  ${jam}jam,  ${rope}rope,  ${sec}varkyan` ;
+});
+
+// function convertTime() {
+//     let timeInput = document.getElementById("timeInput").value;
+//     let date = new Date(timeInput);
+  
+//     let options = {
+//       year: "numeric",
+//       month: "long",
+//       day: "numeric",
+//       hour: "numeric",
+//       minute: "numeric",
+//       second: "numeric",
+//       timeZoneName: "short"
+//     };
+//     let formattedTime = date.toLocaleString("en-US", options);
+  
+//     let resultElement = document.getElementById("result");
+//     resultElement.innerText = formattedTime;
+//   }
+  
+
+
+
+
+
+
+
+
+
+
 
 
 
