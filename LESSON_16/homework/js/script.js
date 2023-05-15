635b7ce7fc4c66d63f7a2622ee7f6367a577ae89
19Dav91 <dmatevosyan91@mail.ru>
2023-05-15 19:16:44 +0400

    Das

--- LESSON_16/homework/js/script.js
+++ LESSON_16/homework/js/script.js
@@ -79,3 +79,84 @@
 // 		}
 // 	}
 // });
+
+// 279.1 Дан инпут и абзац. По изменению инпута выведите его текст в абзац.
+
+// let inp = document.querySelector('#inp');
+// let txt = document.querySelector('#txt');
+// inp.addEventListener('change', function () {
+//     txt.textContent = this.value;
+// });
+
+// 279.2 Дан чекбокс. По изменению чебокса выведите на экран его новое состояние.
+
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('click',func1);
+// function func1(){
+//     console.log(elem.checked);
+// }
+
+// 279.3 Дан инпут. По его изменению проверьте, количество символов в нем меньше 5-ти или нет. 
+// Если меньше - покрасьте границу инпута в зеленый цвет, а если больше - в красный.
+
+// let elem = document.querySelector('#elem');
+// elem.addEventListener('change', func1);
+// function func1() {
+//     if (elem.value.length < 5) {
+//         elem.style.border = '3px solid green'
+//     } else {
+//         elem.style.border = '3px solid red'
+
+//     }
+// }
+
+// 280.1 Дан инпут. Пусть в него вводится текст. 
+// Как только длина текста достигнет 5-ти, выведите на экран сообщение об этом.
+
+// let elem = document.querySelector('#elem');
+// let txt = document.querySelector('#txt');
+// elem.addEventListener('input', func1);
+// function func1() {
+//     if (elem.value.length == 5) {
+//         txt.textContent = 'Նիշերի քանակը հասել է 5-ի';
+//         elem.style.border = '3px solid red'
+//         elem.disabled = true;
+//     }
+// }
+
+// 280.2 Дан инпут. Пусть в него разрешено ввести 5 символов. Дан также абзац. По мере ввода 
+// символов в инпут пишите, сколько еще символов можно ввести. Когда количество символов 
+// превысит 5-ти, то в абзац выводите то, на сколько символов превышена длина текста.
+
+// let elem = document.querySelector('#elem');
+// let txt = document.querySelector('#txt');
+// elem.addEventListener('input', func1);
+// function func1() {
+//     let sum = 5 - elem.value.length;
+//     if (sum >= 0) {
+//         txt.textContent = `Մնացել է ${sum} սիմվոլ`;
+//     } else {
+//         txt.textContent = `Գերազանցել եք ${Math.abs(sum)} սիմվոլ`;
+//     }
+// }
+
+
+// 281.1 Даны два инпута. Сделайте так, чтобы после ввода двух символов фокус ввода 
+// переходил ко второму инпуту, а после ввода двух символов в этот инпут - фокус из него убирался.
+
+let elem1 = document.querySelector('#elem1');
+let elem2 = document.querySelector('#elem2');
+elem1.addEventListener('input', func1);
+elem2.addEventListener('input', func2);
+function func1() {
+    if (elem1.value.length >=2) {
+        elem2.focus();
+        console.log(elem1.value);
+    };
+};
+function func2() {
+    if (elem2.value.length >=2) {
+        elem2.blur();
+        console.log(elem1.value);
+    };
+};
