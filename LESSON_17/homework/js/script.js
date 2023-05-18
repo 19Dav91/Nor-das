d2a2c9687a723d33f69ce5498292edfc00c420de
19Dav91 <dmatevosyan91@mail.ru>
2023-05-18 21:03:12 +0400

    Das

--- LESSON_17/homework/js/script.js
+++ LESSON_17/homework/js/script.js
@@ -8,15 +8,15 @@
 // elem.addEventListener('click', func);
 // elem.addEventListener('dblclick', func);
 
-function func(e) {
-    if (e.type === 'click') {
-        elem.style.backgroundColor = 'green';
-        elem.style.color = 'white'
-    } else if (e.type === 'dblclick') {
-        elem.style.backgroundColor = 'red';
-        elem.style.color = 'white'
-    }
-}
+// function func(e) {
+//     if (e.type === 'click') {
+//         elem.style.backgroundColor = 'green';
+//         elem.style.color = 'white'
+//     } else if (e.type === 'dblclick') {
+//         elem.style.backgroundColor = 'red';
+//         elem.style.color = 'white'
+//     }
+// }
 
 // 419.1   Пусть у вас есть список ul с тегами li:
 // Привяжите к тегу ul обработчик клика. В этом обработчике 
@@ -25,21 +25,21 @@ function func(e) {
 // восклицательный знак. А если клик был по тегу ul - выведите 
 // информацию об этом в консоль.
 
-// let elem = document.querySelector('#elem');
-// elem.addEventListener('click', func1);
-// function func1(e) {
-//     if (e.target.tagName === 'LI') {
-//         e.target.textContent += '!'
-//     } else {
-//         let paragraph = document.createElement('p');
-//         paragraph.innerHTML = 'Դուք սեխմել եք UL-ի վրա';
-//         document.elem.prepend(paragraph);
-//         setTimeout(() => {
-//             paragraph.remove();
-//           }, 800);
-//           paragraph.style.textAlign = 'center'
-//     }
-// }
+let elem = document.querySelector('#elem');
+elem.addEventListener('click', func1);
+function func1(e) {
+    if (e.target.tagName === 'LI') {
+        e.target.textContent += '!'
+    } else {
+        let paragraph = document.createElement('p');
+        paragraph.innerHTML = 'Դուք սեխմել եք UL-ի վրա';
+        elem.prepend(paragraph);
+        setTimeout(() => {
+            paragraph.remove();
+          }, 800);
+          paragraph.style.textAlign = 'center'
+    }
+}
 //  420.1 Сделайте инпут, который будет по вводу выводить значения введенных клавиш и их коды.
 
 // let elem = document.querySelector('#elem');
