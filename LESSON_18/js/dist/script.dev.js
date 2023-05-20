48c3c933f29415651c0aa99593c55ee768d5a55e
19Dav91 <dmatevosyan91@mail.ru>
2023-05-20 14:25:49 +0400

    Das

--- LESSON_18/js/dist/script.dev.js
+++ LESSON_18/js/dist/script.dev.js
@@ -1,23 +1,22 @@
+// let inp1 = document.querySelector('#inp1');
+// let inp2 = document.querySelector('#inp2');
+// let txt = document.querySelector('#txt');
+// let plus = document.querySelector('#plus');
+// let minus = document.querySelector('#minus');
+// let angam = document.querySelector('#angam');
+// let bajanac = document.querySelector('#bajanac');
+// // inp1.addEventListener('input',func1);
+// // inp2.addEventListener('input',func1);
+// plus.addEventListener('click', () => {
+//     txt.textContent = +inp1.value + +inp2.value
+// });
+// minus.addEventListener('click', () => {
+//     txt.textContent = +inp1.value - +inp2.value
+// });
+// angam.addEventListener('click', () => {
+//     txt.textContent = +inp1.value * +inp2.value
+// });
+// bajanac.addEventListener('click', () => {
+//     txt.textContent = +inp1.value / +inp2.value
+// });
 "use strict";
-
-var inp1 = document.querySelector('#inp1');
-var inp2 = document.querySelector('#inp2');
-var txt = document.querySelector('#txt');
-var plus = document.querySelector('#plus');
-var minus = document.querySelector('#minus');
-var angam = document.querySelector('#angam');
-var bajanac = document.querySelector('#bajanac'); // inp1.addEventListener('input',func1);
-// inp2.addEventListener('input',func1);
-
-plus.addEventListener('click', function () {
-  txt.textContent = +inp1.value + +inp2.value;
-});
-minus.addEventListener('click', function () {
-  txt.textContent = +inp1.value - +inp2.value;
-});
-angam.addEventListener('click', function () {
-  txt.textContent = +inp1.value * +inp2.value;
-});
-bajanac.addEventListener('click', function () {
-  txt.textContent = +inp1.value / +inp2.value;
-});
--- LESSON_18/js/script.js
+++ LESSON_18/js/script.js
@@ -1,21 +1,22 @@
-let inp1 = document.querySelector('#inp1');
-let inp2 = document.querySelector('#inp2');
-let txt = document.querySelector('#txt');
-let plus = document.querySelector('#plus');
-let minus = document.querySelector('#minus');
-let angam = document.querySelector('#angam');
-let bajanac = document.querySelector('#bajanac');
-// inp1.addEventListener('input',func1);
-// inp2.addEventListener('input',func1);
-plus.addEventListener('click', () => {
-    txt.textContent = +inp1.value + +inp2.value
-});
-minus.addEventListener('click', () => {
-    txt.textContent = +inp1.value - +inp2.value
-});
-angam.addEventListener('click', () => {
-    txt.textContent = +inp1.value * +inp2.value
-});
-bajanac.addEventListener('click', () => {
-    txt.textContent = +inp1.value / +inp2.value
-});
+// let inp1 = document.querySelector('#inp1');
+// let inp2 = document.querySelector('#inp2');
+// let txt = document.querySelector('#txt');
+// let plus = document.querySelector('#plus');
+// let minus = document.querySelector('#minus');
+// let angam = document.querySelector('#angam');
+// let bajanac = document.querySelector('#bajanac');
+// // inp1.addEventListener('input',func1);
+// // inp2.addEventListener('input',func1);
+// plus.addEventListener('click', () => {
+//     txt.textContent = +inp1.value + +inp2.value
+// });
+// minus.addEventListener('click', () => {
+//     txt.textContent = +inp1.value - +inp2.value
+// });
+// angam.addEventListener('click', () => {
+//     txt.textContent = +inp1.value * +inp2.value
+// });
+// bajanac.addEventListener('click', () => {
+//     txt.textContent = +inp1.value / +inp2.value
+// });
+ 
