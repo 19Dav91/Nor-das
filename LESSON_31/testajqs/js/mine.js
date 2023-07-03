3a4057bb69baaab0d6f140d4916a2de4822ed802
19Dav91 <dmatevosyan91@mail.ru>
2023-07-03 08:11:33 +0400

    Das

--- LESSON_31/testajqs/js/mine.js
+++ LESSON_31/testajqs/js/mine.js
@@ -1,22 +1,22 @@
-window.addEventListener("DOMContentLoaded", () => {
-    function req() {
-        const request = new XMLHttpRequest();
-        request.open("GET", "http://localhost:3000")
-        request.setRequestHeader("Content-type", "application/json;charset=utf-8");
-        request.send();
-        request.addEventListener("readystatechange", function () {
-            if (request.readyState === 4 && request.status == 200) {
-                let data = JSON.parse(request.response);
-                console.log(data);
-            } else {
-                console.error("stoped")
-            }
-        });
-    }
+// window.addEventListener("DOMContentLoaded", () => {
+//     function req() {
+//         const request = new XMLHttpRequest();
+//         request.open("GET", "http://localhost:3000")
+//         request.setRequestHeader("Content-type", "application/json;charset=utf-8");
+//         request.send();
+// //         request.addEventListener("readystatechange", function () {
+// //             if (request.readyState === 4 && request.status == 200) {
+// //                 let data = JSON.parse(request.response);
+// //                 console.log(data);
+// //             } else {
+// //                 console.error("stoped")
+// //             }
+// //         });
+// //     }
 
 
-    req();
-})
+// //     req();
+// // })
 
 
 // request.addEventListener("load", function () {
@@ -35,27 +35,46 @@ window.addEventListener("DOMContentLoaded", () => {
 // })
 
 
-function createCards(response) {
-    response.forEach(item => {
-        let card = document.createElement("div");
-        card.classList.add("card");
-        let icon;
-        if (item.sex === "male") {
-            icon = "icons/mars.png"
-        } else {
-            icon = "icons/female.png"
-        };
-        card.innerHTML = `
-    <img src="${item.photo}"alt="">
-    <div class="name">${item.name}${item.surname}</div>
+// function createCards(response) {
+//     response.forEach(item => {
+//         let card = document.createElement("div");
+//         card.classList.add("card");
+//         let icon;
+//         if (item.sex === "male") {
+//             icon = "icons/mars.png"
+//         } else {
+//             icon = "icons/female.png"
+//         };
+//         card.innerHTML = `
+//     <img src="${item.photo}"alt="">
+//     <div class="name">${item.name}${item.surname}</div>
 
-    <div class="sex">
-    <img src=${icon} alt="">
-    </div>
-    <div class="age">${item.age}</div>
-    `;
-        document.querySelector(".app").appendChild(card);
-    })
-}
+//     <div class="sex">
+//     <img src=${icon} alt="">
+//     </div>
+//     <div class="age">${item.age}</div>
+//     `;
+//         document.querySelector(".app").appendChild(card);
+//     })
+// }
 
-createCards()
+// createCards()
+window.addEventListener("DOMContentLoaded", () => {
+    function req() {
+        const request = new XMLHttpRequest();
+        request.open("GET", "http://localhost:3000/people")
+        request.setRequestHeader("Content-type", "application/json;charset=utf-8");
+        request.send();
+        request.addEventListener("readystatechange", function () {
+            if (request.readyState === 4 && request.status == 200) {
+                let data = JSON.parse(request.response);
+                console.log(data);
+            } else {
+                console.error("stoped")
+            }
+        });
+    }
+
+
+    req();
+})
