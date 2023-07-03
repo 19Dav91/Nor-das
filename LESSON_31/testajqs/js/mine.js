daa0f4684a91262d7aa8684848ed84ef7a13ba45
19Dav19 <dmatevosyan91@mail.ru>
2023-07-03 09:55:08 +0400

    Das

--- LESSON_31/testajqs/js/mine.js
+++ LESSON_31/testajqs/js/mine.js
@@ -1,38 +1,38 @@
 window.addEventListener("DOMContentLoaded", () => {
     function req() {
         const request = new XMLHttpRequest();
-        request.open("GET", "http://localhost:3000/people")
+        request.open("GET", "http://localhost:3000")
         request.setRequestHeader("Content-type", "application/json;charset=utf-8");
         request.send();
-//         request.addEventListener("readystatechange", function () {
-//             if (request.readyState === 4 && request.status == 200) {
-//                 let data = JSON.parse(request.response);
-//                 console.log(data);
-//             } else {
-//                 console.error("stoped")
-//             }
-//         });
-//     }
-
-
-//     req();
-// })
-
-
-request.addEventListener("load", function () {
-    if (request.status == 200) {
-        let data = JSON.parse(request.response);
-        console.log(data);
-        createCards(data)
-
-    } else {
-        console.error("stoped")
+        request.addEventListener("readystatechange", function () {
+            if (request.readyState === 4 && request.status == 200) {
+                let data = JSON.parse(request.response);
+                console.log(data);
+            } else {
+                console.error("stoped")
+            }
+        });
     }
-});
 
-} //req()
-document.querySelector("button").addEventListener("click", req, )
+
+    req();
 })
+
+
+// request.addEventListener("load", function () {
+//     if (request.status == 200) {
+//         let data = JSON.parse(request.response);
+//         console.log(data);
+//         createCards(data)
+
+//     } else {
+//         console.error("stoped")
+//     }
+// });
+
+// } //req()
+// document.querySelector("button").addEventListener("click", req, )
+// })
 
 
 function createCards(response) {
