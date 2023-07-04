75cdac2ea5f2b00cf4bfdeea537295f4607b1620
19Dav19 <dmatevosyan91@mail.ru>
2023-07-04 21:48:45 +0400

    Das

--- LESSON_31/testajqs/js/fetch.js
+++ LESSON_31/testajqs/js/fetch.js
@@ -1,55 +1,55 @@
-window.addEventListener("DOMContentLoaded", () => {
-    function req() {
-        const request = new XMLHttpRequest();
-        request.open("GET", "http://localhost:3000/people")
-        request.setRequestHeader("Content-type", "application/json;charset=utf-8");
-        request.send();
-        async function getResource(url) {
-            const res = await axios(`${url}`);
-            if (res.status !== 200) {
-                throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-            }
-            return res;
-        }
-        getResource("http://localhost:3000/people")
-            .then(data => createCards(data.data)) ///console.log
-            .catch(err => console.error(err))
-        async function getResource(url) {
-            const res = await fetch(`${url}`);
-            if (!res.ok) {
-                throw new Error(`Could not fetch ${url}, status: ${res.status}`)
-            }
-            return await res.json()
-        }
-        this.remove();
+// window.addEventListener("DOMContentLoaded", () => {
+//     function req() {
+//         const request = new XMLHttpRequest();
+//         request.open("GET", "http://localhost:3000/people")
+//         request.setRequestHeader("Content-type", "application/json;charset=utf-8");
+//         request.send();
+//         async function getResource(url) {
+//             const res = await axios(`${url}`);
+//             if (res.status !== 200) {
+//                 throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+//             }
+//             return res;
+//         }
+//         getResource("http://localhost:3000/people")
+//             .then(data => createCards(data.data)) ///console.log
+//             .catch(err => console.error(err))
+//         async function getResource(url) {
+//             const res = await fetch(`${url}`);
+//             if (!res.ok) {
+//                 throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+//             }
+//             return await res.json()
+//         }
+//         this.remove();
 
-        getResource("http://localhost:3000/people")
-            .then(data => createCards(data))
-            .catch(err => console.error(err))
+//         getResource("http://localhost:3000/people")
+//             .then(data => createCards(data))
+//             .catch(err => console.error(err))
         
-    }
-    document.querySelector("button").addEventListener("click", req, { "once": true })
-})
+//     }
+//     document.querySelector("button").addEventListener("click", req, { "once": true })
+// })
 
-function createCards(response) {
-    response.forEach(item => {
-        let card = document.createElement("div");
-        card.classList.add("card");
-        let icon;
-        if (item.sex === "male") {
-            icon = "icons/mars.png"
-        } else {
-            icon = "icons/female.png"
-        };
-        card.innerHTML = `
-    <img src="${item.photo}"alt="">
-    <div class="name">${item.name}${item.surname}</div>
+// function createCards(response) {
+//     response.forEach(item => {
+//         let card = document.createElement("div");
+//         card.classList.add("card");
+//         let icon;
+//         if (item.sex === "male") {
+//             icon = "icons/mars.png"
+//         } else {
+//             icon = "icons/female.png"
+//         };
+//         card.innerHTML = `
+//     <img src="${item.photo}"alt="">
+//     <div class="name">${item.name}${item.surname}</div>
 
-    <div class="sex">
-    <img src=${icon} alt="">
-    </div>
-    <div class="age">${item.age}</div>`;
-        document.querySelector(".app").appendChild(card);
-    })
-}
+//     <div class="sex">
+//     <img src=${icon} alt="">
+//     </div>
+//     <div class="age">${item.age}</div>`;
+//         document.querySelector(".app").appendChild(card);
+//     })
+// }
 
---
+++ LESSON_31/testajqs/js/post.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("form");
+window.addEventListener("DOMContentLoaded", () => {
+    function req(e) {
+        e.preventDefault();
+        let formData = new FormData(form);
+        console.log(formData);
+        formData.append("id", Math.random());
+        let obj = {};
+        formData.forEach((value, key) => {
+            obj[key] = value;
+        })
+        let json = JSON.stringify(obj);
+        const request = new XMLHttpRequest();
+        request.open("POST", "http://localhost:3000/people")
+        request.setRequestHeader("Content-type", "application/json;charset=utf-8");
+        request.send(json);
+        request.addEventListener("load", function () {
+            if (request.status == 200) {
+                let data = JSON.parse(request.response);
+                console.log(data);
+            } else {
+                console.error("stoped")
+            }
+        })
+    }
+    document.querySelector("button").addEventListener("click", req, { "once": true })
+})
