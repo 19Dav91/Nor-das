205867d0b8c8177d38ad6d11d2d0ced65e297eb8
19Dav19 <dmatevosyan91@mail.ru>
2023-07-08 11:31:33 +0400

    Das

--- js/LESSON_31/testajqs/js/postFatch.js
+++ js/LESSON_31/testajqs/js/postFatch.js
@@ -1,53 +1,53 @@
 
-window.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("form");
-    function req(e) {
-        e.preventDefault();
-        let formData = new FormData(form);
-        console.log(formData);
-        formData.append("id", Math.random());
+// // window.addEventListener("DOMContentLoaded", () => {
+// //     const form = document.querySelector("form");
+// //     function req(e) {
+// //         e.preventDefault();
+// //         let formData = new FormData(form);
+// //         console.log(formData);
+// //         formData.append("id", Math.random());
 
-        let obj = {};
-        formData.forEach((value, key) => {
-            obj[key] = value;
-        })
-        //         let json = JSON.stringify(obj);
-        //         const request = new XMLHttpRequest();
-        //         request.open("POST", "http://localhost:3000/people")
-        //         request.setRequestHeader("Content-type", "application/json;charset=utf-8");
-        //         request.send(json);
-        //         request.addEventListener("load", function () {
-        //             if (request.status == 200) {
-        //                 let data = JSON.parse(request.response);
-        //                 console.log(data);
-        //             } else {
-        //                 console.error("stoped")
-        //             }
-        //         })
-        //     }
-            document.querySelector("button").addEventListener("click", req, { "once": true })
-        // })
-        async function getResource(url, data) {
-            const res = await fetch(`${url}`, {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json"
+// //         let obj = {};
+// //         formData.forEach((value, key) => {
+// //             obj[key] = value;
+// //         })
+//         //         let json = JSON.stringify(obj);
+//         //         const request = new XMLHttpRequest();
+//         //         request.open("POST", "http://localhost:3000/people")
+//         //         request.setRequestHeader("Content-type", "application/json;charset=utf-8");
+//         //         request.send(json);
+//         //         request.addEventListener("load", function () {
+//         //             if (request.status == 200) {
+//         //                 let data = JSON.parse(request.response);
+//         //                 console.log(data);
+//         //             } else {
+//         //                 console.error("stoped")
+//         //             }
+//         //         })
+//         //     }
+//             document.querySelector("button").addEventListener("click", req, { "once": true })
+//         // })
+//         async function getResource(url, data) {
+//             const res = await fetch(`${url}`, {
+//                 method: "POST",
+//                 headers: {
+//                     "Content-type": "application/json"
 
-                },
-                body: JSON.stringify(data)
-            });
-            if (!res.ok) {
-                throw new Error(`Could not fetch ${url},status:${res.status}`)
-            }
-            return await res.json()
-        }
-        getResource("http://localhost:3000/people", obj)
-            .then(data => console.log(data))
-            .catch(err => console.error(err));
-    // 
-    axios.post("http://localhost:3000/people",obj);
-}
+//                 },
+//                 body: JSON.stringify(data)
+//             });
+//             if (!res.ok) {
+//                 throw new Error(`Could not fetch ${url},status:${res.status}`)
+//             }
+//             return await res.json()
+//         }
+//         getResource("http://localhost:3000/people", obj)
+//             .then(data => console.log(data))
+//             .catch(err => console.error(err));
+//     // 
+//     axios.post("http://localhost:3000/people",obj);
+// }
 
-    document.querySelector("button").addEventListener("click", req, { "once": true })
+//     document.querySelector("button").addEventListener("click", req, { "once": true })
 
-})
+// })
--- js/LESSON_32/js/vxod.js
+++ js/LESSON_32/js/vxod.js
@@ -1,43 +1,11 @@
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-  event.preventDefault();
-
-  // Assuming you have an input field with id "userId" where the user enters the desired user ID
-  const user = document.getElementById('user').value;
-
-  axios.get(`http://localhost:3000/people/${user}`)
-    .then(function (response) {
-      const userData = response.data; // Assuming the response contains data for a single user
-      console.log(userData);
-
-      // Perform further actions with the user data here
-    })
-    .catch(function (error) {
-      console.error('Ошибка при отправке данных на сервер:', error);
-    });
-});
-
-
-
-
-
-// document.getElementById('loginForm').addEventListener('submit', function (event) {
-//   event.preventDefault();
-
-
- 
-//   axios.get('http://localhost:3000/people')
-//     .then(function (response) {
-//       const data = response.data;
-//       console.log(data);
-
-      
-//     })
-//     .catch(function (error) {
-//       console.error('Ошибка при отправке данных на сервер:', error);
-//     });
-// });
-// if (data.success) {
-      //   console.log('Пользователь вошел в систему:', data.data.people);
-      // } else {
-      //   console.log('Ошибка входа:', data.message);
-      // }
+const axios = require('axios');
+// import axios from '../../../node_modules/axios/dist/axios.js';
+axios.get(url, { params })
+  .then(response => {
+    // Обработка успешного ответа
+    console.log(response.data);
+  })
+  .catch(error => {
+    // Обработка ошибки
+    console.error(error);
+  });
