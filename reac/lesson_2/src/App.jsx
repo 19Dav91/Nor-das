d0fefab6b4c4207a446e30d12b266b83b6571baa
19Dav91 <dmatevosyan91@gmail.com>
2023-09-11 10:57:33 +0400

    as

--- reac/lesson_2/src/App.jsx
+++ reac/lesson_2/src/App.jsx
@@ -11,7 +11,7 @@ import './App.css';
 //         }
 //         return sum
 //     }
-//     return getSum(1523);
+//     return getSum(123);
 // }
 
 // 32.1 Используя созданную вами функцию getDigitsSum выведите в абзац сумму цифр числа 12345.
@@ -37,11 +37,11 @@ import './App.css';
 // 	function show1() {
 // 		alert(1);
 // 	}
-	
+
 // 	function show2() {
 // 		alert(2);
 // 	}
-	
+
 // 	return <div>
 // 		<button onClick={show1}>act1</button>
 // 		<button onClick={show2}>act2</button>
@@ -52,11 +52,11 @@ import './App.css';
 // 34.1 Сделайте так, чтобы по клику на первую кнопку алертом выводилось число 1, 
 // по клику на вторую кнопку - число 2, а по клику на третью - число 3.
 // export default function App() {
-    
+
 //         function showMess(num) {
 //             alert(num);
 //         }
-    
+
 // 	return <div>
 // 		<button onClick={()=> showMess(1)}>act1</button>
 // 		<button onClick={()=> showMess(2)}>act2</button>
@@ -70,8 +70,126 @@ import './App.css';
 // 		console.log(event);
 // 		// console.log(event.target);
 // 	}
-	
+
 // 	return <div>
 // 		<button onClick={func}>act</button>
 // 	</div>;
 // }
+
+
+//11.09.23 lesson_3
+
+// export default function App() {
+//     function func(event, ...arg) {
+//         console.log(arg, event);
+//     }
+
+//     return <div>
+//         <button onClick={event => func(event, 'eee', 'Dav')}>act</button>
+//     </div>;
+// }
+
+// export default function App() {
+//     const arr = [1, 2, 3];
+//     return <div>
+//         {arr}
+//     </div>;
+// }
+
+// export default function App() {
+//     const arr = [
+//         <li key='1'>1</li>,
+//         <li key='2'>2</li>,
+//         <li key='3'>3</li>,
+//         <li key='4'>4</li>,
+//         <li key='5'>5</li>,
+//     ];
+//     return <ul>
+//         {arr}
+//     </ul>
+// }
+
+// export default function App() {
+//     const arr = [];
+
+//     for (let i = 0; i <= 9; i++) {
+//         arr.push(<p key={i}>{i}</p>);
+//     }
+
+//     return <div>
+//         {arr}
+//     </div>;
+// }
+
+
+// export default function App() {
+//     const arr = [1, 2, 3, 4, 5];
+//     const res = [];
+
+//     for (const elem of arr) {
+//         res.push(<p key={elem}>{elem}</p>);
+//     }
+
+//     return <div>
+//         {res}
+//     </div>;
+// }
+
+// export default function App() {
+// 	const arr = [1, 2, 3, 4, 5];
+	
+// 	const res = arr.map(function(item,index) {
+// 		return <p key={index}>{item}</p>;
+// 	});
+	
+// 	return <div>
+// 		{res}
+// 	</div>;
+// }
+// const prods = [
+// 	{name: 'product1', cost: 100},
+// 	{name: 'product2', cost: 200},
+// 	{name: 'product3', cost: 300},
+// ];
+
+// export default function App() {
+// 	const res = prods.map(function(item) {
+// 		return <p key={item.name}>{item.name} {item.cost}</p>;
+// 	});
+	
+// 	return <div>
+// 		{res}
+// 	</div>;
+// }
+
+const users = [
+	{name: 'user1', surn: 'surn1', age: 30},
+	{name: 'user2', surn: 'surn2', age: 31},
+	{name: 'user3', surn: 'surn3', age: 32},
+];
+
+// export default function App(){
+//     const res = users.map(function(item,index){
+//         return <li key={index}>
+//             <span>{item.name}</span>
+//             <span>{item.surn}</span>
+//             <span>{item.age}</span>
+//         </li>
+//     })
+//     return <ul>
+//         {res}
+//     </ul>
+// }
+
+export default function App(){
+    const res = users.map((item,index)=>{
+        return <li key={index}>
+            <span>{item.name}</span>
+            <span>{item.surn}</span>
+            <span>{item.age}</span>
+        </li>
+    })
+    return <ul>
+        {res}
+    </ul>
+}
