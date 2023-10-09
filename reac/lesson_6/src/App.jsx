8f90427d8c39a82300820595f0eb0b88dae6833e
19Dav91 <dmatevosyan91@mail.ru>
2023-10-09 21:10:57 +0400

    Das

--- reac/lesson_6/src/App.jsx
+++ reac/lesson_6/src/App.jsx
@@ -115,14 +115,14 @@ import { useState } from 'react';
 //     </>
 // };
 
-export default function App() {
-    const [checked, setChecked] = useState(true);
+// export default function App() {
+//     const [checked, setChecked] = useState(true);
 	
-	return <div>
-		<input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}  />
-        <p>состояние: {checked ? 'отмечен' : 'не отмечен'}</p>
-	</div>;
-};
+// 	return <div>
+// 		<input type="checkbox" checked={checked} onChange={() => setChecked(!checked)}  />
+//         <p>состояние: {checked ? 'отмечен' : 'не отмечен'}</p>
+// 	</div>;
+// };
 
 // export default function App() {
 // 	const [checked, setChecked] = useState(true);
