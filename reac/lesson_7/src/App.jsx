fc21770f18216427285e2d2c38b21c8c5604b99a
19Dav91 <dmatevosyan91@mail.ru>
2023-10-18 19:39:19 +0400

    фыв

--- reac/lesson_7/src/App.jsx
+++ reac/lesson_7/src/App.jsx
@@ -12,8 +12,7 @@ function App() {
                 </p>
                 <a
                     className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
+                    href="https://reactjs.org" target="_blank"
                     rel="noopener noreferrer"
                 >
                     Learn React
