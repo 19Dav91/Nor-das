f51b09b9055585b2ed60454e10277377274ada54
19Dav91 <dmatevosyan91@mail.ru>
2023-11-15 23:23:07 +0400

    DAs

--- reac/lesson_8/src/pages/Layout.js
+++ reac/lesson_8/src/pages/Layout.js
@@ -1,25 +1,23 @@
 import { Outlet, Link } from "react-router-dom";
 
 const Layout = () => {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Outlet />
+    return <>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/blogs">Blogs</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+        <Outlet />
     </>
-  )
+
 };
 
 export default Layout;
