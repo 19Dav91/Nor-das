1913d9337984d36f68a570b00fc8fcc0b4c043a9
19Dav91 <dmatevosyan91@mail.ru>
2023-11-19 15:16:27 +0400

    Das

--- reac/lesson_9/src/App.jsx
+++ reac/lesson_9/src/App.jsx
@@ -25,24 +25,56 @@
 // export default App;
 
 // import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPage";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
 
-export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="blogs" element={<Blogs />} />
-                    <Route path="contact" element={<Contact />} />
-                    <Route path="*" element={<NoPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
-}
+// export default function App() {
+//     return (
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path="/" element={<Layout />}>
+//                     <Route index element={<Home />} />
+//                     <Route path="blogs" element={<Blogs />} />
+//                     <Route path="contact" element={<Contact />} />
+//                     <Route path="*" element={<NoPage />} />
+//                 </Route>
+//             </Routes>
+//         </BrowserRouter>
+//     );
+// }
+import { useEffect, useState } from 'react';
+
+// export default function App() {
+// 	useEffect(() => {
+// 		document.body.style.backgroundColor = 'green';
+//         console.log('render');
+// 	}, []);
+
+// 	return (
+// 		<div>
+// 			<h1>React App</h1>
+// 		</div>
+// 	);
+// }
+
+
+// export default function App() {
+//     const [color, setColor] = useState('green');
+//     useEffect(() => {
+//         document.body.style.backgroundColor = color;
+//         console.log('render');
+//     }, [color]);
+//     function changeColor() {
+//         setColor('orange');
+//     }
+//     return (
+//         <div>
+//             <h1>React App</h1>
+//             <button onClick={changeColor}>change</button>
+//         </div>
+//     );
+// };
