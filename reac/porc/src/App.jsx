f4745e8c86e6a7770c5dd59c9fb27d3335af3fa6
19Dav91 <dmatevosyan91@mail.ru>
2023-11-25 23:30:58 +0400

    adsa

--- reac/porc/src/App.jsx
+++ reac/porc/src/App.jsx
@@ -1,21 +1,72 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import { styles } from "./styles";
+import { IconButton } from "rsuite"; 
+import { Admin, Menu, Reload, Resize, Search,Plus,Completed } from '@rsuite/icons'; 
+// Import the default CSS 
+import "rsuite/dist/rsuite.min.css"; 
 
 function App() {
     const [input, setinput] = useState('');
+    const [notes, setNotes] = useState([]);
     const [status, setstatus] = useState('All');
+    let subHendler = (e) => {
+        e.preventDefault();
+
+    };
+    function addNote() {
+
+        if (input !== '') {
+            setNotes([...notes, input]);
+            setinput('');
+        }
+    }
+
     //     useEffect(()=>{
 
     //     return ()=>{ /* cleanup code */ }
     //          },[])
+    const res = notes.map((note) => {
+        return <>
+            <form >
+                <div style={styles.class1}>
+                    {note}
+                </div>
+                <button style={styles.class2} onClick={subHendler}>
+                    <i className="fas fa-plus-square"></i>
+                </button>
+            </form>
+        </>
+    });
 
     return <>
-        <div className='todo-container'>
-            <input className='input todo' type='text' />
-            <button className='button todo ' onClick={(e) => { }} >Add</button>
+        <div className='App'>
+            <header>
+                <h1>Todo List</h1>
+            </header>
+            <div className='todo-container'>
+                <input
+                    className='input todo'
+                    type='text'
+                    value={input}
+                    onChange={(e) => setinput(e.target.value)} />
+                <button className='button' onClick={addNote} >
+                    <IconButton icon={<Plus />} color="cyan" 
+                appearance="primary"  /> 
+                <IconButton icon={<Completed />} color="cyan" 
+                appearance="primary"  /> 
+                </button>
+            </div>
         </div>
+        <p
+            style={
+                {
+                    color: 'black'
+                }}>{res}
+        </p>
 
     </>
 }
 
 export default App;
+
---
+++ reac/porc/src/styles.js
@@ -0,0 +1,41 @@
+
+const class1 = {
+    backgroundColor: 'white',
+    listStyle: 'none',
+    textAlign: 'center',
+    padding: '10px',
+    margin: '10px auto',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    width: '50%',
+    justifyContent: 'space-between',
+}
+const class2 = {
+    backgroundColor: 'greenyellow',
+    overflow: 'hidden',
+    color: 'black',
+    listStyle: 'none',
+    textAlign: 'center',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    border: 'none',
+
+};
+const class3 = {
+    backgroundColor: 'white',
+    listStyle: 'none',
+    textAlign: 'center',
+    padding: '10px',
+    margin: '10px auto',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    width: '50%',
+    justifyContent: 'center',
+};
+export const styles = {
+    class1: class1,
+    class2: class2,
+    class3: class3,
+};
