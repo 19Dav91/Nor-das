2933991ceb267b5d19755b1de532afa981376e87
19Dav91 <dmatevosyan91@mail.ru>
2023-12-03 16:58:12 +0400

    AFv

--- reac/testOne/src/App.jsx
+++ reac/testOne/src/App.jsx
@@ -2,20 +2,20 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
+
 export default function App() {
-
     const [inputText, setinputText] = useState('');
-
     const [notes, setnotes] = useState(() => {
         const savednotes = localStorage.getItem('notes');
         return savednotes ? JSON.parse(savednotes) : [];
     });
-    const [status, setstatus] = useState('all');
+    const [status, setstatus] = useState('All');
+
     useEffect(() => {
         localStorage.setItem('notes', JSON.stringify(notes));
     }, [notes]);
 
-    return <>
+    return (
         <div className='App'>
             <header>
                 <h1>Todo List</h1>
@@ -28,7 +28,6 @@ export default function App() {
                     setnotes={setnotes}
                     setstatus={setstatus}
                 />
-
             </div>
             <TodoList
                 notes={notes}
@@ -36,46 +35,5 @@ export default function App() {
                 status={status}
             />
         </div>
-    </>
+    );
 };
-
-
-// import React, { useEffect, useState } from 'react';
-// import './App.css';
-// import Form from './components/Form';
-// import TodoList from './components/TodoList';
-
-// export default function App() {
-//     const [inputText, setinputText] = useState('');
-//     const [notes, setnotes] = useState(() => {
-//         const savednotes = localStorage.getItem('notes');
-//         return savednotes ? JSON.parse(savednotes) : [];
-//     });
-//     const [status, setstatus] = useState('all'); // Ensure initial status matches options and is lowercase
-
-//     useEffect(() => {
-//         localStorage.setItem('notes', JSON.stringify(notes));
-//     }, [notes]);
-
-//     return (
-//         <div className='App'>
-//             <header>
-//                 <h1>Todo List</h1>
-//             </header>
-//             <div>
-//                 <Form
-//                     inputText={inputText}
-//                     setinputText={setinputText}
-//                     notes={notes}
-//                     setnotes={setnotes}
-//                     setstatus={setstatus} // Pass setstatus prop
-//                 />
-//             </div>
-//             <TodoList
-//                 notes={notes}
-//                 setnotes={setnotes}
-//                 status={status}
-//             />
-//         </div>
-//     );
-// };
--- reac/testOne/src/components/TodoList.js
+++ reac/testOne/src/components/TodoList.js
@@ -3,22 +3,24 @@ import '../App.css'
 import { styles } from "../styles";
 import { Icon } from '@iconify/react';
 
-const TodoList = ({ notes, setnotes, status }) => {
+const TodoList = ({ notes, setnotes, status }) => { 
     const deleteNote = (index) => {
         const newNotes = [...notes];
         newNotes.splice(index, 1);
         setnotes(newNotes);
     };
+
     const completeNote = (index) => {
         const newNotes = [...notes];
         newNotes[index].completed = !newNotes[index].completed;
         newNotes[index].backgroundColor = newNotes[index].completed ? 'green' : 'red';
         setnotes(newNotes);
     };
+
     const filterNotes = notes.filter((note) => {
-        if (status === 'All') return true;
-        if (status === 'Completed') return note.completed;
-        if (status === 'Uncompleted') return !note.completed;
+        if (status === 'all') return true;
+        if (status === 'completed') return note.completed;
+        if (status === 'uncompleted') return !note.completed;
         return true;
     });
 
@@ -31,63 +33,16 @@ const TodoList = ({ notes, setnotes, status }) => {
                 </button>
                 <button onClick={() => deleteNote(index)}>
                     <Icon className='todo-button  btn' icon="wpf:delete" color="red" />
-
                 </button>
             </div>
         </div>
     ));
-    return <>
-        {res}
-    </>
-}
+
+    return (
+        <>
+            {res}
+        </>
+    );
+};
+
 export default TodoList;
-
-
-// import React from "react";
-// import '../App.css'
-// import { styles } from "../styles";
-// import { Icon } from '@iconify/react';
-
-// const TodoList = ({ notes, setnotes, status }) => { // Removed setstatus from props as it's not needed here
-//     const deleteNote = (index) => {
-//         const newNotes = [...notes];
-//         newNotes.splice(index, 1);
-//         setnotes(newNotes);
-//     };
-
-//     const completeNote = (index) => {
-//         const newNotes = [...notes];
-//         newNotes[index].completed = !newNotes[index].completed;
-//         newNotes[index].backgroundColor = newNotes[index].completed ? 'green' : 'red';
-//         setnotes(newNotes);
-//     };
-
-//     const filterNotes = notes.filter((note) => {
-//         if (status === 'all') return true;
-//         if (status === 'completed') return note.completed;
-//         if (status === 'uncompleted') return !note.completed;
-//         return true;
-//     });
-
-//     const res = filterNotes.map((note, index) => (
-//         <div className='todo-container bg' style={{ ...styles.class1, backgroundColor: note.backgroundColor }} key={index}>
-//             <p className={note.completed ? 'completed' : ''}>{note.text}</p>
-//             <div>
-//                 <button onClick={() => completeNote(index)}>
-//                     <Icon className='todo-button  btn' icon="fluent-mdl2:completed-solid" color="green" />
-//                 </button>
-//                 <button onClick={() => deleteNote(index)}>
-//                     <Icon className='todo-button  btn' icon="wpf:delete" color="red" />
-//                 </button>
-//             </div>
-//         </div>
-//     ));
-
-//     return (
-//         <>
-//             {res}
-//         </>
-//     );
-// };
-
-// export default TodoList;
